afea34047a59ad06dfd326b1e813e6a9d37c9f38
lilyFortuneTime <lily.chen@maxfortune.tw>
2019-11-19 11:41:19 +0800

    create new project 'season'

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">
-                        Nice blog post!</div>
+            <ApprovalCard>
+                <div>
+                <h4>Warning!</h4>
+                Are you sure want to do this?    
                 </div>
-            </div>           
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    avatar={faker.image.avatar()} 
+                    author="Sam" 
+                    timeAgo="Today at 4:49PM" 
+                    content="頭香爽辣"
+                />
+            </ApprovalCard>
+            <CommentDetail 
+                avatar={faker.image.avatar()} 
+                author="Alex" 
+                timeAgo="Today at 2:00AM" 
+                content="五樓母胎單"
+            />
+            <CommentDetail 
+                avatar={faker.image.avatar()} 
+                author="Jane" 
+                timeAgo="Yestuday at 5:00PM" 
+                content="你們都歪樓了"
+            />
         </div>
     );
 };
