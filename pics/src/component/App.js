6c8e728e4d1ca39eae2b3a572e71b3e6ca89170c
lilyFortuneTime <lily.chen@maxfortune.tw>
2020-01-07 10:19:41 +0800

    [Complate] picture list

--- pics/src/component/App.js
+++ pics/src/component/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
+
 
 class App extends React.Component {
   state = {
@@ -8,14 +10,10 @@ class App extends React.Component {
   };
 
   onSearchSubmit = async (term) => {
-    const response = await axios.get('https://api.unsplash.com/search/photos',{
+    const response = await unsplash.get('/search/photos',{
       params: {
         query: term
-      },
-      headers: {
-        Authorization: 'Client-ID 76d224391b87e02a12811c877bcca2fc045d70f2b49ff4e3e6a240c8b5110ede'
-      }
-    });
+      }});
 
     this.setState({
       images: response.data.results
@@ -26,7 +24,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ margin: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images}/>
       </div>
     );
   }
